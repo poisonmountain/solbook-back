494eb37f5200f8aed9f702abe365a56603858ceb
ghktndyd <hwasuyong99@naver.com>
2025-01-21 15:19:13 +0900

    init project

---
+++ src/main/java/org/solbook/SolbookApplication.java
@@ -0,0 +1,13 @@
+package org.solbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolbookApplication.class, args);
+    }
+
+}
