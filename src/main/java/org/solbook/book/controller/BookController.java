6eaad10235d244aee01d98af1b52ac81307737df
ComNewbie <iforking3@naver.com>
2025-01-31 22:26:56 +0900

    feat: 책 상세 API 구현 BookController : 책 id를 통해 해당 책의 상세 정보 불러옴 BookService : 책의 정보를 BookResponse 로 바꾸어 전달 BookResponse : 책의 필요한 상세 정보를 담는 DTO

--- src/main/java/org/solbook/book/controller/BookController.java
+++ src/main/java/org/solbook/book/controller/BookController.java
@@ -3,12 +3,10 @@ package org.solbook.book.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.solbook.book.controller.request.BookRequest;
+import org.solbook.book.controller.response.BookResponse;
 import org.solbook.book.service.BookService;
 import org.solbook.common.response.JsonResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/book")
@@ -21,4 +19,12 @@ public class BookController {
         bookService.saveBook(request);
         return JsonResult.successOf();
     }
+
+    @GetMapping("/{bookId}")
+    public JsonResult<?> getBook(@PathVariable("bookId") Long bookId) {
+        BookResponse bookResponse = bookService.getBook(bookId);
+        return JsonResult.successOf(bookResponse);
+    }
+
+
 }
