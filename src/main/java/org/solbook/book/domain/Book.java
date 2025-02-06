b6a723b8e0192da30b7da6d5234f3aff9f9c8fc9
ComNewbie <iforking3@naver.com>
2025-02-06 23:31:29 +0900

    refactor: 퀴즈와 책 연관관계 생성 Book : 퀴즈와 OneToMany 관계 Quiz : 책과 ManyToOne 관계

--- src/main/java/org/solbook/book/domain/Book.java
+++ src/main/java/org/solbook/book/domain/Book.java
@@ -3,6 +3,10 @@ package org.solbook.book.domain;
 import jakarta.persistence.*;
 import lombok.*;
 import org.solbook.common.auditing.BaseEntity;
+import org.solbook.quiz.domain.Quiz;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
@@ -24,6 +28,14 @@ public class Book extends BaseEntity {
 
     @Column(nullable = false)
     private String image;
+
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Quiz> quizzes = new ArrayList<>();
+
+    public void addQuiz(Quiz quiz) {
+        quizzes.add(quiz);
+        quiz.setBook(this);
+    }
 
     @Builder
     public Book(String title, String author, String image){
